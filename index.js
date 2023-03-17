a42228baf524b5abf0c60db53cb4407cf22b77c1
Ernest <ernestwinata@gmail.com>
2023-03-17 08:15:59 -0700

    creates myfiles.md

--- index.js
+++ index.js
@@ -1,2 +1,30 @@
 
-// Write your code here
+// num1*num2 = 62
+// function multiply(num1, num2) {
+//     return num1*num2 === 62
+// }
+
+// function random() {
+//     return math.random()
+// }
+
+// function mod() {
+//     let mod = num3/num4
+//     return mod = 4
+// }
+
+// function max() {
+//     math.max(17,18,19,20);
+//     return max
+// }
+let num1 = 2;
+let num2 = 31;
+let multiply = num1*num2
+
+let num3 = 12;
+let num4 = 3;
+let mod = num3/num4
+
+let max = Math.max(17, 19, 20);
+
+let random = Math.floor(Math.random() * 10);
